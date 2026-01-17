b98d9c221b19956008f41c87d0397e049b25e835
yyoonjiwon <yyoonjiwon@gmail.com>
2026-01-17 05:38:19 +0000

    Initial commit with Hello World

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// Add JS here
+import './style.css';
+
+const app = document.querySelector('#app');
+app.innerHTML = `
+  <h1>Hello World!</h1>
+  <a href="https://vitejs.dev" target="_blank">Documentation</a>
+`;
